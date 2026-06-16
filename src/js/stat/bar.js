4855a75cc375c5aa4bed5f150bbbfd6fe8176298
Fan-iX <61686936+Fan-iX@users.noreply.github.com>
2026-06-16 05:10:45 -0400

    implement bar dodge and stack

--- src/js/stat/bar.js
+++ src/js/stat/bar.js
@@ -5,7 +5,7 @@ import { intraaction } from '#base/js/utils'
  *   { x } => { x, y, height }
  *   { y } => { x, y, width }
  */
-export default Object.assign(function (data) {
+export default Object.assign(function (data, { position = "stack", width = 1, height = 1 }) {
     if (data.x != null && data.y != null)
         throw new Error(`"StatBar" only supports "x" or "y", not both`)
     let values = data.x ?? data.y
@@ -20,16 +20,43 @@ export default Object.assign(function (data) {
     let $raw = Array.from(groups.values()), cates = Array.from(groups.keys())
     let result = {
         $raw: $raw,
-        count: $raw.map(x => x.length),
         $group: cates.map(x => x.group),
-        value: cates.map(x => x.value),
     }
     for (let key of keys) {
         result[key] = cates.map(x => x.group).map(i => group.categories[i][key])
     }
     if (data.x) {
-        return (({ value, count, ...etc }) => ({ x: value, y: count.map(x => x / 2), height: count, ...etc }))(result)
+        result.x = cates.map(v => v.value)
+        result.height = $raw.map(v => v.length)
     } else {
-        return (({ value, count, ...etc }) => ({ y: value, x: count.map(x => x / 2), width: count, ...etc }))(result)
+        result.y = cates.map(v => v.value)
+        result.width = $raw.map(v => v.length)
     }
+    if (position == "dodge") {
+        if (data.x) {
+            result.y = result.height.map(v => v / 2)
+            result.width = cates.map((_, i) => width / group.categories.length)
+            result.xnudge = result.$group.map((v, i) => (result.xnudge?.[i] ?? 0) + ((v + 0.5) / group.categories.length - 0.5) * width)
+        } else {
+            result.x = result.width.map(v => v / 2)
+            result.height = cates.map((_, i) => height / group.categories.length)
+            result.ynudge = result.$group.map((v, i) => (result.ynudge?.[i] ?? 0) + ((v + 0.5) / group.categories.length - 0.5) * height)
+        }
+    } else {
+        let tmp = Object.fromEntries(Array.from(new Set(values)).map(v => [v, 0]))
+        let offset = cates.map(_ => 0)
+        let indices = cates.map((cate, i) => [cate, i]).sort(([{ group: a }], [{ group: b }]) => a - b)
+        for (let [cate, i] of indices) {
+            offset[i] = tmp[cate.value]
+            tmp[cate.value] += $raw[i].length
+        }
+        if (data.x) {
+            result.y = result.height.map((v, i) => offset[i] + v / 2)
+            result.width = cates.map(_ => width)
+        } else {
+            result.x = result.width.map((v, i) => offset[i] + v / 2)
+            result.height = cates.map(_ => height)
+        }
+    }
+    return result
 }, { core_attrs: ['x', 'y', 'xnudge', 'ynudge'] })
