6898132931a8f439df118a152bf8737fdbec8440
Fan-iX <61686936+Fan-iX@users.noreply.github.com>
2026-07-11 02:33:02 -0400

    place plot axes behind layers

--- src/core/CorePlot.vue
+++ src/core/CorePlot.vue
@@ -720,6 +720,14 @@ const axes = computed(() => {
             <CoreGridV :breaks="gridBreaks.v" :layout="innerRect" :activeTransform="activeTransform"
                 :coord2pos="coord2pos" :transition="transition" />
         </g>
+        <g :transform="`translate(${panel.left}, ${panel.top})`">
+            <CoreAxis v-for="axis in axes.filter(a => typeof a.bind.position !== 'number')" v-bind="axis.bind"
+                v-on="axis.on" v-model:transition="transition" :activeTransform="activeTransform" />
+            <g :clip-path="props.clip ? `url(#${vid}-plot-clip)` : null">
+                <CoreAxis v-for="axis in axes.filter(a => typeof a.bind.position === 'number')" v-bind="axis.bind"
+                    v-on="axis.on" v-model:transition="transition" :activeTransform="activeTransform" />
+            </g>
+        </g>
         <g :transform="`translate(${panel.left}, ${panel.top})`"
             :clip-path="props.clip ? `url(#${vid}-plot-clip)` : null">
             <g v-bind="transformBind" :style="{ transition }">
@@ -733,14 +741,6 @@ const axes = computed(() => {
             <CoreSelection :coord2pos="coord2pos" :pos2coord="pos2coord" :layout="innerRect"
                 :modelValue="selectionPreview" :transition="transition" :activeTransform="activeTransform"
                 :theme="_selectionPreviewTheme" :flip />
-        </g>
-        <g :transform="`translate(${panel.left}, ${panel.top})`">
-            <CoreAxis v-for="axis in axes.filter(a => typeof a.bind.position !== 'number')" v-bind="axis.bind"
-                v-on="axis.on" v-model:transition="transition" :activeTransform="activeTransform" />
-            <g :clip-path="props.clip ? `url(#${vid}-plot-clip)` : null">
-                <CoreAxis v-for="axis in axes.filter(a => typeof a.bind.position === 'number')" v-bind="axis.bind"
-                    v-on="axis.on" v-model:transition="transition" :activeTransform="activeTransform" />
-            </g>
         </g>
         <slot></slot>
         <foreignObject v-if="props.legendTeleport">
